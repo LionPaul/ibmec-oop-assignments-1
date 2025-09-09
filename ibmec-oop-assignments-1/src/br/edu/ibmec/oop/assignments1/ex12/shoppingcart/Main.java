c1ef05ee5c612e86770c10197eba633d6ffaca72
LionPaul <ptalregister@gmail.com>
2025-09-09 17:28:32 -0300

    update

---
+++ ibmec-oop-assignments-1/src/br/edu/ibmec/oop/assignments1/ex12/shoppingcart/Main.java
@@ -0,0 +1,25 @@
+package br.edu.ibmec.oop.assignments1.ex12.shoppingcart;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Main {
+    
+ public static void main (String[] args) {
+	 
+	 Scanner sc = new Scanner(System.in);
+	 String resposta = "s";
+	 String nome;
+	 double valor;
+	 ArrayList<Product> arrayProduct = new ArrayList<Product>();
+	 
+	 while (resposta.equals("s")) {
+		 System.out.println("Name: ");
+		 name = sc.nextLine();
+		 
+	 }
+	 
+	 
+ }
+ 
+}
---
+++ ibmec-oop-assignments-1/src/br/edu/ibmec/oop/assignments1/ex12/shoppingcart/Product.java
@@ -0,0 +1,9 @@
+package br.edu.ibmec.oop.assignments1.ex12.shoppingcart;
+
+public class Product {
+
+ private String name;
+ private double value;
+ 
+ public Product(String name, )
+}
