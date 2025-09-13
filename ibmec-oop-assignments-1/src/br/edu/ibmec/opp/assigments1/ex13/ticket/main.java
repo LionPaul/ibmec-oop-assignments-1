055c1042fd5d2245a1b459a4dac44c1aee07ff42
LionPaul <ptalregister@gmail.com>
2025-09-13 16:47:24 -0300

    update

---
+++ ibmec-oop-assignments-1/src/br/edu/ibmec/opp/assigments1/ex13/ticket/main.java
@@ -0,0 +1,10 @@
+package br.edu.ibmec.opp.assigments1.ex13.ticket;
+
+public class main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ ibmec-oop-assignments-1/src/br/edu/ibmec/opp/assigments1/ex13/ticket/ticket.java
@@ -0,0 +1,5 @@
+package br.edu.ibmec.opp.assigments1.ex13.ticket;
+
+public class ticket {
+
+}
