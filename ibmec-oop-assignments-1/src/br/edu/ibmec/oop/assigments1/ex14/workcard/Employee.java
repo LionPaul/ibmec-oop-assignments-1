205932c23a577dcd1cedb9fc03247bfee2cf51f2
LionPaul <ptalregister@gmail.com>
2025-09-23 10:48:05 -0300

    Update

---
+++ ibmec-oop-assignments-1/src/br/edu/ibmec/oop/assigments1/ex14/workcard/Employee.java
@@ -0,0 +1,18 @@
+package br.edu.ibmec.oop.assigments1.ex14.workcard;
+
+public class Employee {
+	
+	private String name;
+	private String position;
+	private double salary;
+	private WorkCard card;
+	
+	public Employee(String name, String position, double salary, WorkCard card) {
+		this.name = name;
+		this.position = position;
+		this.salary = salary;
+		this.card = card;
+	}
+	
+
+}
---
+++ ibmec-oop-assignments-1/src/br/edu/ibmec/oop/assigments1/ex14/workcard/WorkCard.java
@@ -0,0 +1,9 @@
+package br.edu.ibmec.oop.assigments1.ex14.workcard;
+
+public class WorkCard {
+
+	private int number;
+	private String issueDate;
+	private String issuingBody;
+	
+}
